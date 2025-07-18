b1c20b00f13cf6feec71d7fb855f77034cb5e543
danielp-crypto <76240023+danielp-crypto@users.noreply.github.com>
2025-07-18 20:01:34 +0200

    Pre-select grade and country in student info edit form based on user data

--- studentinfo-edit.php
+++ studentinfo-edit.php
@@ -81,9 +81,9 @@ $selectedLocation = $student['location'] ?? '';
       <label>What grade are you in?</label>
       <select class="form-select" name="user-type" required>
         <option value="">-- Grade --</option>
-        <option>High School</option>
-        <option>University/College</option>
-        <option>Other</option>
+        <option value="High School"<?= ($student['user_type'] === 'High School') ? ' selected' : '' ?>>High School</option>
+        <option value="University/College"<?= ($student['user_type'] === 'University/College') ? ' selected' : '' ?>>University/College</option>
+        <option value="Other"<?= ($student['user_type'] === 'Other') ? ' selected' : '' ?>>Other</option>
       </select>
     </div>
     <div class="mb-3">
@@ -102,33 +102,33 @@ $selectedLocation = $student['location'] ?? '';
       <label for="location">Country </label>
       <select class="form-select" name="location" required>
         <option value="">-- Select Country --</option>
-        <option>Argentina</option>
-        <option>Australia</option>
-        <option>Brazil</option>
-        <option>Canada</option>
-        <option>China</option>
-        <option>Colombia</option>
-        <option>France</option>
-        <option>Germany</option>
-        <option>India</option>
-        <option>Indonesia</option>
-        <option>Italy</option>
-        <option>Japan</option>
-        <option>Malaysia</option>
-        <option>Mexico</option>
-        <option>Netherlands</option>
-        <option>Nigeria</option>
-        <option>Russia</option>
-        <option>Saudi Arabia</option>
-        <option>Singapore</option>
-        <option>South Africa</option>
-        <option>South Korea</option>
-        <option>Spain</option>
-        <option>Sweden</option>
-        <option>Turkey</option>
-        <option>United Arab Emirates</option>
-        <option>United Kingdom</option>
-        <option>United States</option>
+        <option value="Argentina"<?= ($student['location'] === 'Argentina') ? ' selected' : '' ?>>Argentina</option>
+        <option value="Australia"<?= ($student['location'] === 'Australia') ? ' selected' : '' ?>>Australia</option>
+        <option value="Brazil"<?= ($student['location'] === 'Brazil') ? ' selected' : '' ?>>Brazil</option>
+        <option value="Canada"<?= ($student['location'] === 'Canada') ? ' selected' : '' ?>>Canada</option>
+        <option value="China"<?= ($student['location'] === 'China') ? ' selected' : '' ?>>China</option>
+        <option value="Colombia"<?= ($student['location'] === 'Colombia') ? ' selected' : '' ?>>Colombia</option>
+        <option value="France"<?= ($student['location'] === 'France') ? ' selected' : '' ?>>France</option>
+        <option value="Germany"<?= ($student['location'] === 'Germany') ? ' selected' : '' ?>>Germany</option>
+        <option value="India"<?= ($student['location'] === 'India') ? ' selected' : '' ?>>India</option>
+        <option value="Indonesia"<?= ($student['location'] === 'Indonesia') ? ' selected' : '' ?>>Indonesia</option>
+        <option value="Italy"<?= ($student['location'] === 'Italy') ? ' selected' : '' ?>>Italy</option>
+        <option value="Japan"<?= ($student['location'] === 'Japan') ? ' selected' : '' ?>>Japan</option>
+        <option value="Malaysia"<?= ($student['location'] === 'Malaysia') ? ' selected' : '' ?>>Malaysia</option>
+        <option value="Mexico"<?= ($student['location'] === 'Mexico') ? ' selected' : '' ?>>Mexico</option>
+        <option value="Netherlands"<?= ($student['location'] === 'Netherlands') ? ' selected' : '' ?>>Netherlands</option>
+        <option value="Nigeria"<?= ($student['location'] === 'Nigeria') ? ' selected' : '' ?>>Nigeria</option>
+        <option value="Russia"<?= ($student['location'] === 'Russia') ? ' selected' : '' ?>>Russia</option>
+        <option value="Saudi Arabia"<?= ($student['location'] === 'Saudi Arabia') ? ' selected' : '' ?>>Saudi Arabia</option>
+        <option value="Singapore"<?= ($student['location'] === 'Singapore') ? ' selected' : '' ?>>Singapore</option>
+        <option value="South Africa"<?= ($student['location'] === 'South Africa') ? ' selected' : '' ?>>South Africa</option>
+        <option value="South Korea"<?= ($student['location'] === 'South Korea') ? ' selected' : '' ?>>South Korea</option>
+        <option value="Spain"<?= ($student['location'] === 'Spain') ? ' selected' : '' ?>>Spain</option>
+        <option value="Sweden"<?= ($student['location'] === 'Sweden') ? ' selected' : '' ?>>Sweden</option>
+        <option value="Turkey"<?= ($student['location'] === 'Turkey') ? ' selected' : '' ?>>Turkey</option>
+        <option value="United Arab Emirates"<?= ($student['location'] === 'United Arab Emirates') ? ' selected' : '' ?>>United Arab Emirates</option>
+        <option value="United Kingdom"<?= ($student['location'] === 'United Kingdom') ? ' selected' : '' ?>>United Kingdom</option>
+        <option value="United States"<?= ($student['location'] === 'United States') ? ' selected' : '' ?>>United States</option>
       </select>
     </div>
     <button type="submit" class="btn btn-primary">Save</button>
