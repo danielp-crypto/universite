8ed4428d6c383b938015fba39a63a9c394088d2a
danielp-crypto <76240023+danielp-crypto@users.noreply.github.com>
2026-04-09 12:18:48 +0200

    improved local backend

--- api.py
+++ api.py
@@ -4,6 +4,10 @@ import requests
 import os
 import jwt
 from jwt import PyJWKClient
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
