1c2f27302c5ae7b597bfce090e0565a11f761c57
daniel <dphutiagae@icloud.com>
2026-07-05 11:54:27 -0700

    install capacitor dependencies

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.universite.app',
+  appName: 'Universite',
+  webDir: 'public'
+};
+
+export default config;
